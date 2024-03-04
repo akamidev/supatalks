1e267eb91266fc930d409db200e66f1dd924184b
Mehdi <akamimehdi.dev@gmail.com>
2024-03-04 16:06:37 +0100

    importation du framework CSS Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,6 @@
 import './bootstrap.js';
+import './bootstrap.bundle.min.js';
+
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -6,5 +8,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/bootstrap.min.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
