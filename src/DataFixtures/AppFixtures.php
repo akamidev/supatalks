5ec223cb127bd20bca78a1e639fd2155ac51db37
Mehdi <akamimehdi.dev@gmail.com>
2024-03-05 12:51:10 +0100

    Creation des fixtures pour les enement

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,61 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use DateTime;
+use Faker\Factory;
+use App\Entity\Event;
+use App\Entity\Speaker;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR'); // initialisation de faker
+        $events = [
+            'Frontend Masters',
+            'Backend Masters',
+            'Truth about PHP',
+            'Symfony 7, what\'s new ?',
+            'React, framework or library ?',
+            'Vue.js, the new challenger ?',
+            'Angular, the old one ?',
+            'Web components, the future ?',
+            'WebAssembly, how to use it ?',
+            'GPDR, how to be compliant ?',
+            'Docker, the new way to deploy ?',
+            'Kubernetes, scale like a boss ?',
+            'AWS, the cloud leader ?',
+            'Wordpress, still alive ?',
+            'PHP 8.3, what\'s new ?',
+            'UI/UX, for frontend developers',
+            'API, the essential for frontend',
+            'GraphQL vs REST, which is better ?',
+            'Web security, how to protect ?',
+            'Web performance, how to optimize ?',
+        ];
+        foreach ($events as $item) {
+            $event = new Event();
+            $event->setName($item)
+                ->setTheme('Web development')
+                ->setDate($faker->dateTimeBetween('-6 months', '+6 months'))
+                ->setLocation($faker->city)
+                ->setAttendee($faker->numberBetween(10, 100))
+                ->setPrice($faker->numberBetween(0, 250))
+                ;
+            $manager->persist($event);
+        }
+
+         $event = new Event();
+         $event->setName('Frontend Masters')
+         ->setTheme('Web development')
+         ->setDate(new DateTime('2021-10-10'))
+         ->setLocation('paris')
+         ->setAttendee(30)
+         ->setPrice(100.00)
+         ;
+         $manager->persist($event);
 
         $manager->flush();
     }
