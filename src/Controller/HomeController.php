7bacfa3a41c697c35c87e22d2bc105b25dbd8e8d
Mehdi <akamimehdi.dev@gmail.com>
2024-03-05 17:03:46 +0100

    Récuperation de tous les évenement et l'intervenant

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EventRepository;
+use App\Repository\SpeakerRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_home')]
+    public function index(
+        EventRepository $eventRepository,
+        SpeakerRepository $speakerRepository
+    ): Response
     {
         return $this->render('home/index.html.twig', [
             'title' => 'Supatalks',
+            'events' => $eventRepository->findAll(),
+            'speakers' => $speakerRepository->findAll(),
         ]);
     }
 }
